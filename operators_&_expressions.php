d2ff850e152fff448a3a5cebff8c7049e5d2dcd1
wpthemesmith <mtsrakibul@gmail.com>
2025-04-26 13:37:17 +0600

    Arithmetic Operators

---
+++ operators_&_expressions.php
@@ -0,0 +1,25 @@
+<?php
+
+//Arithmetic Operators
+$one=1;
+$two="2";
+$three=3;
+$four= "4";
+$five= 5;
+
+echo "<h1>Arithmetic Operators</h1>";
+echo "<br>";
+echo "<h3>Addition</h3>";
+echo $two+$three;
+echo "<br>";
+echo "<h3>Subtraction</h3>";
+echo $four-$two;
+echo "<br>";
+echo "<h3>Multiplication</h3>";
+echo $two*$four;
+echo "<br>";
+echo "<h3>Division</h3>";
+echo $four/$two;
+echo "<br>";
+echo "<h3>Modulus</h3>";
+echo $five%$three;
