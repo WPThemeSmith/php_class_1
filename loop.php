c9b1a8c0b982322b79b3e12370b9b522fe0c4eb0
wpthemesmith <mtsrakibul@gmail.com>
2025-04-29 14:49:12 +0600

    Foreach loop

--- loop.php
+++ loop.php
@@ -6,4 +6,11 @@ echo "<br>";
 echo "<h3>For Loop</h3>";
 for ($x=0; $x <= 10; $x++) {
     echo "The number is: $x <br>";
+}  
+echo "<br>";
+
+echo "<h3>For Each Loop</h3>";
+$colors = array("red", "green", "blue", "yellow");
+foreach ($colors as $value) {
+    echo "$value <br>";
 }
